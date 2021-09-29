709345e15b0e3923e6235bf0bce9ac8cd3c99d1f
AconCavy <aconcavy@gmail.com>
2021-09-30 07:33:02 +0900

    Add venv files

---
+++ setup.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+set venv_name=venv
+
+if not exist %venv_name% (
+    echo "Start to setup"
+    python -m venv %venv_name%
+    "%venv_name%/Scripts/activate.bat"
+    pip install -r requirements.txt
+    deactivate
+)
---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+DIR=$(cd $(dirname $0); pwd)
+cd $DIR
+
+VENV_NAME=venv
+
+if [ ! -d $VENV_NAME ]; then
+  echo "Start to setup"
+  python3 -m venv $VENV_NAME
+  . $VENV_NAME/bin/activate
+  pip install -r requirements.txt
+  deactivate
+fi
