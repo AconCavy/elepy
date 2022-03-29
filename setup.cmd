13828e55d79a1da8af32df395ff8a3cce7e08e34
AconCavy <aconcavy@gmail.com>
2022-03-30 04:36:47 +0900

    Modify setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -2,12 +2,13 @@
 
 set venv_name=venv
 
-if not exist %venv_name% (
-    echo "Start to setup"
-    python -m venv %venv_name%
-    "%venv_name%/Scripts/activate.bat"
-    pip install -r requirements.txt
-    deactivate
-)
+echo Start to setup
 
-npm install
+if not exist %venv_name% (
+    python -m venv %venv_name%
+)
+call %venv_name%/Scripts/activate.bat ^
+    && pip install -r requirements.txt ^
+    && deactivate
+
+call npm install
